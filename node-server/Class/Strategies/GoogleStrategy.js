96d2e701d67f3d05c36d077f2f16cc4d389aa250
skrilax91 <leo_brossard@hotmail.fr>
2022-10-11 23:48:30 +0200

    adding many things

---
+++ node-server/Class/Strategies/GoogleStrategy.js
@@ -0,0 +1,45 @@
+const Strategy = require("../prototype/Strategy");
+const { google } = require('googleapis');
+
+class GoogleStrategy extends Strategy {
+    static uid = "google_strategy";
+    config;
+    auth;
+    scopes;
+
+    constructor(configs) {
+        this.config = configs
+        this.auth = new google.auth.OAuth2(this.config.clientId, this.config.secret, this.config.callback);
+        this.scopes = [];
+        return this.auth;
+    }
+
+    getAuthUrl() {
+        if (!this.scopes.length)
+            return null;
+
+        return this.auth.generateAuthUrl({
+            access_type: 'offline',
+            scope: this.scopes,
+            include_granted_scopes: true
+        });
+    }
+
+    getAuthenticator() {
+        return this.auth;
+    }
+
+    static authenticate() {
+
+    }
+
+    static verify() {
+
+    }
+
+    static isAuthenticated(id) {
+        
+    }
+}
+
+module.exports = GoogleStrategy;

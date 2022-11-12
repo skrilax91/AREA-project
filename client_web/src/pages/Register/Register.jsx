01dc73ee7f8ad1d772ff6eff43e2ac5d485f7b60
David <david.gozlan@epitech.eu>
2022-11-12 14:53:30 +0100

    :sparkles: register added

--- client_web/src/pages/Register/Register.jsx
+++ client_web/src/pages/Register/Register.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../components/header.module.css";
 import style from "../Login/login.module.css";
 import Header from "../../components/Header";
 import registerRequest from "./register";
+import {useNavigate} from "react-router-dom";
 
 function Content() {
+    let navigate = useNavigate();
+
+    const [message, setMessage] = useState("");
+
     let handleSubmit = (event) => {
         event.preventDefault();
         let email = event.target[0].value;
         let password = event.target[1].value;
         let rememberMe = event.target[2].value;
-        registerRequest(email, password, rememberMe);
+        let data = registerRequest(email, password, rememberMe)
+            .then(async response => {
+                const data = await response.json();
+
+                if (!response.ok) {
+                    const error = (data && data.message) || response.statusText;
+                    setMessage("Error : " + error);
+                    return Promise.reject(error);
+                }
+                setMessage("Account created !");
+
+            });
     }
 
     return (
@@ -19,6 +35,7 @@ function Content() {
             <form className={style.loginForm} onSubmit={handleSubmit}>
                 <input className={style.loginInput} type="email" placeholder="Email"/>
                 <input className={style.loginInput} type="password" placeholder="Password"/>
+                <h1>{message}</h1>
                 <button className={style.loginButton} type="submit">Log in</button>
             </form>
         </section>);
--- client_web/src/pages/Register/register.js
+++ client_web/src/pages/Register/register.js
@@ -1,22 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function registerRequest(email, password) {
     return fetch("http://localhost:8080/api/auth/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
         },
         body: JSON.stringify({
             username: email,
             password: password,
         })
-    })
-        .then(data => {
-            if (data.ok) {
-                console.log("Registration successful");
-                return data.json();
-            }
-            return data;
-        });
+    });
 }
 
